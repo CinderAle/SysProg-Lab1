2b3ea7d3d9a5b464ddca90f98fffd6c394f1e6b1
CinderAle <a.d.strukov@gmail.com>
2023-09-18 10:21:58 +0300

    Acceleratot tables added

--- Lab1_1/main.cpp
+++ Lab1_1/main.cpp
@@ -1,5 +1,6 @@
 #include <windows.h>
 #include <gdiplus.h>
+#include "resource.h"
 
 #define MAIN_WINDOW_CLASS L"MainWindow"
 
@@ -14,6 +15,7 @@
 #define WINDOW_LEFT_BORDER 500
 #define WINDOW_WIDTH 800
 #define MAIN_WINDOW_STYLE (WS_OVERLAPPED | WS_VISIBLE | WS_SYSMENU | WS_MINIMIZEBOX | WS_CAPTION)
+#define WINDOW_ICON L"javalogo.bmp"
 
 //Figure physics
 #define FIGURE_DEFAULT_SPEED 10
@@ -22,26 +24,44 @@
 #define FIGURE_BORDER_JUMP 15
 #define FIGURE_WIDTH_CORRECTION 15
 #define FIGURE_HEIGHT_CORRECTION 35
+#define FIGURE_SLEEP_AFTER_HIT 50
+
+//Colors
+#define BLACK_COLOR RGB(0,0,0)
+#define DARK_GRAY_COLOR RGB(50,50,50)
+#define RED_COLOR RGB(255,100,100)
+#define BLUE_COLOR RGB(100,100,255)
+#define YELLOW_COLOR RGB(208,224,83)
+#define GREEN_COLOR RGB(83,224,125)
+#define WHITE_COLOR RGB(255,255,255)
 
 //Figure styles
 #define FIGURE_WIDTH 200
 #define FIGURE_HEIGHT 100
 #define FIGURE_START_X (WINDOW_WIDTH / 2 - FIGURE_WIDTH / 2 - 15)
 #define FIGURE_START_Y (WINDOW_HEIGHT / 2 - FIGURE_HEIGHT / 2 - 25)
-#define BLACK_COLOR RGB(0,0,0)
-#define BACKGROUND_COLOR RGB(50,50,50)
-#define DARK_GRAY_COLOR RGB(50,50,50)
-#define RED_COLOR RGB(255, 100, 100)
-#define FIGURE_SPRITE_COLOR RGB(100,100,255)
-#define FIGURE_IMAGE_SRC L"dorik1.bmp"
+#define BACKGROUND_COLOR DARK_GRAY_COLOR
+#define FIGURE_SPRITE_COLOR YELLOW_COLOR
+#define FIGURE_IMAGE_SRC L"dorik.bmp"
 #define MAX_FIGURE_WIDTH (WINDOW_WIDTH - 50)
 #define MAX_FIGURE_HEIGHT (WINDOW_HEIGHT - 50)
+#define FIGURE_HIT_BORDER_WIDTH 2
+#define FIGURE_HIT_BORDER_COLOR WHITE_COLOR
 
-void ShowFigureJump(HWND hWnd);
+//Auto motion constants
+#define INCREASE_COORDINATE 1
+#define DECREASE_COORDINATE -1
+#define ACCELERATION_VK 0x58
+#define AUTO_MOTION_SLEEP 5
+
+void ShowFigureJump(HWND hWnd, int windowWidth, int windowHeight);
 
 struct {
 	int x, y, width, height;
 } FigureCoordinates;
+
+int horizontalMotion = 0;
+int verticalMotion = 0;
 
 int constexpr CheckDimension(int dimension, int space, int max) noexcept
 {
@@ -71,18 +91,22 @@ int CorrectDimension(int dimension, int space, int max) noexcept
 
 void ChangeFigureCoordinates(HWND hWnd, int x, int y)
 {
-	if (CheckDimension(FigureCoordinates.x + x, FigureCoordinates.width, WINDOW_WIDTH - FIGURE_WIDTH_CORRECTION) && CheckDimension(FigureCoordinates.y + y, FigureCoordinates.height, WINDOW_HEIGHT - FIGURE_HEIGHT_CORRECTION))
+	RECT windowSize;
+	GetWindowRect(hWnd, &windowSize);
+	const int windowWidth = windowSize.right - windowSize.left;
+	const int windowHeight = windowSize.bottom - windowSize.top;
+	if (CheckDimension(FigureCoordinates.x + x, FigureCoordinates.width, windowWidth - FIGURE_WIDTH_CORRECTION) && CheckDimension(FigureCoordinates.y + y, FigureCoordinates.height, windowHeight - FIGURE_HEIGHT_CORRECTION))
 	{
 		FigureCoordinates.x += x;
 		FigureCoordinates.y += y;
 	}
 	else
 	{
-		FigureCoordinates.x = CorrectDimension(FigureCoordinates.x + x, FigureCoordinates.width, WINDOW_WIDTH - FIGURE_WIDTH_CORRECTION);
-		FigureCoordinates.y = CorrectDimension(FigureCoordinates.y + y, FigureCoordinates.height, WINDOW_HEIGHT - FIGURE_HEIGHT_CORRECTION);
+		FigureCoordinates.x = CorrectDimension(FigureCoordinates.x + x, FigureCoordinates.width, windowWidth - FIGURE_WIDTH_CORRECTION);
+		FigureCoordinates.y = CorrectDimension(FigureCoordinates.y + y, FigureCoordinates.height, windowHeight - FIGURE_HEIGHT_CORRECTION);
 	}
 	InvalidateRect(hWnd, NULL, TRUE);
-	ShowFigureJump(hWnd);
+	ShowFigureJump(hWnd, windowWidth, windowHeight);
 }
 
 void DrawFigure(HDC hDc, HWND hWnd)
@@ -102,31 +126,39 @@ void DrawFigure(HDC hDc, HWND hWnd)
 	}
 }
 
-void ShowFigureJump(HWND hWnd)
+void ShowFigureJump(HWND hWnd, int windowWidth, int windowHeight)
 {
-	if (!CheckBorder(FigureCoordinates.x, FigureCoordinates.width, WINDOW_WIDTH - FIGURE_WIDTH_CORRECTION))
+	if (!CheckBorder(FigureCoordinates.x, FigureCoordinates.width, windowWidth - FIGURE_WIDTH_CORRECTION))
 	{
-		HPEN borderPen = CreatePen(PS_SOLID, 1, RED_COLOR);
+		HPEN borderPen = CreatePen(PS_SOLID, FIGURE_HIT_BORDER_WIDTH, FIGURE_HIT_BORDER_COLOR);
 		HDC hDc = GetDC(hWnd);
 		SelectObject(hDc, borderPen);
 		DrawFigure(hDc, hWnd);
 		DeleteObject(borderPen);
 		ReleaseDC(hWnd, hDc);
 		FigureCoordinates.x += FigureCoordinates.x == 0 ? FIGURE_BORDER_JUMP : -FIGURE_BORDER_JUMP;
-		Sleep(100);
+		Sleep(FIGURE_SLEEP_AFTER_HIT);
 		InvalidateRect(hWnd, NULL, TRUE);
+		if (horizontalMotion != 0)
+		{
+			horizontalMotion = (horizontalMotion == INCREASE_COORDINATE ? DECREASE_COORDINATE : INCREASE_COORDINATE);
+		}
 	}
-	if (!CheckBorder(FigureCoordinates.y, FigureCoordinates.height, WINDOW_HEIGHT - FIGURE_HEIGHT_CORRECTION))
+	if (!CheckBorder(FigureCoordinates.y, FigureCoordinates.height, windowHeight - FIGURE_HEIGHT_CORRECTION))
 	{
-		HPEN borderPen = CreatePen(PS_SOLID, 1, RED_COLOR);
+		HPEN borderPen = CreatePen(PS_SOLID, FIGURE_HIT_BORDER_WIDTH, FIGURE_HIT_BORDER_COLOR);
 		HDC hDc = GetDC(hWnd);
 		SelectObject(hDc, borderPen);
 		DrawFigure(hDc, hWnd);
 		DeleteObject(borderPen);
 		ReleaseDC(hWnd, hDc);
 		FigureCoordinates.y += FigureCoordinates.y == 0 ? FIGURE_BORDER_JUMP : -FIGURE_BORDER_JUMP;
-		Sleep(100);
+		Sleep(FIGURE_SLEEP_AFTER_HIT);
 		InvalidateRect(hWnd, NULL, TRUE);
+		if (verticalMotion)
+		{
+			verticalMotion = (verticalMotion == INCREASE_COORDINATE ? DECREASE_COORDINATE : INCREASE_COORDINATE);
+		}
 	}
 }
 
@@ -134,57 +166,86 @@ void DrawMainWindow(HWND hWnd)
 {
 	PAINTSTRUCT ps;
 	HDC hDc = BeginPaint(hWnd, &ps);
+
+	HDC hdcBuffer = CreateCompatibleDC(hDc);
+	HBITMAP hbmBuffer = CreateCompatibleBitmap(hDc, WINDOW_WIDTH, WINDOW_HEIGHT);
+	SelectObject(hdcBuffer, hbmBuffer);
+
 	HPEN clearBorder = CreatePen(PS_NULL, 0, BLACK_COLOR);
-	SelectObject(hDc, clearBorder);
+	SelectObject(hdcBuffer, clearBorder);
 	HBRUSH brush = CreateSolidBrush(BACKGROUND_COLOR);
-	SelectObject(hDc, brush);
-	Rectangle(hDc, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-	DrawFigure(hDc, hWnd);
+	SelectObject(hdcBuffer, brush);
+
+	RECT windowSize;
+	GetWindowRect(hWnd, &windowSize);
+
+	Rectangle(hdcBuffer, 0, 0, windowSize.right - windowSize.left, windowSize.bottom - windowSize.top);
+	DrawFigure(hdcBuffer, hWnd);
 	DeleteObject(clearBorder);
 	DeleteObject(brush);
+
+	BitBlt(hDc, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, hdcBuffer, 0, 0, SRCCOPY);
+	DeleteObject(hbmBuffer);
+	DeleteDC(hdcBuffer);
+
 	EndPaint(hWnd, &ps);
+}
+
+void StopAutoMotion() noexcept
+{
+	verticalMotion = 0;
+	horizontalMotion = 0;
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 {
+	int speed = ((GetAsyncKeyState(ACCELERATION_VK) >= 0) ? 0 : FIGURE_ACCELERATION);
 	switch (uMsg) 
 	{
 	case WM_PAINT:
 		DrawMainWindow(hWnd);
 		break;
-	case WM_KEYDOWN: 
-	{
-		int speed;
-		if (GetAsyncKeyState(VK_SPACE) < 0)
+	case WM_COMMAND:
+		speed += FIGURE_DEFAULT_SPEED;
+		switch (LOWORD(wParam))
 		{
-			speed = FIGURE_DEFAULT_SPEED + FIGURE_ACCELERATION;
-		}
-		else
-		{
-			speed = FIGURE_DEFAULT_SPEED;
-		}
-		if(GetAsyncKeyState(VK_UP) < 0)
+		case ID_BTN_UP:
 			ChangeFigureCoordinates(hWnd, 0, -speed);
-		else if (GetAsyncKeyState(VK_DOWN) < 0)
-			ChangeFigureCoordinates(hWnd, 0, speed);
-		else if (GetAsyncKeyState(VK_LEFT) < 0)
+			StopAutoMotion();
+			break;
+		case ID_BTN_LEFT:
 			ChangeFigureCoordinates(hWnd, -speed, 0);
-		else if (GetAsyncKeyState(VK_RIGHT) < 0)
+			StopAutoMotion();
+			break;
+		case ID_BTN_DOWN:
+			ChangeFigureCoordinates(hWnd, 0, speed);
+			StopAutoMotion();
+			break;
+		case ID_BTN_RIGHT:
 			ChangeFigureCoordinates(hWnd, speed, 0);
-		
-	}
+			StopAutoMotion();
+			break;
+		case ID_BTN_W:
+			verticalMotion = (verticalMotion != DECREASE_COORDINATE ? DECREASE_COORDINATE : 0);
+			break;
+		case ID_BTN_A:
+			horizontalMotion = (horizontalMotion != DECREASE_COORDINATE ? DECREASE_COORDINATE : 0);
+			break;
+		case ID_BTN_S:
+			verticalMotion = (verticalMotion != INCREASE_COORDINATE ? INCREASE_COORDINATE : 0);
+			break;
+		case ID_BTN_D:
+			horizontalMotion = (horizontalMotion != INCREASE_COORDINATE ? INCREASE_COORDINATE : 0);
+			break;
+		default:
+			StopAutoMotion();
+			break;
+		}
 		break;
 	case WM_MOUSEWHEEL:
 	{
-		int speed;
-		if (GetAsyncKeyState(VK_SPACE) < 0)
-		{
-			speed = FIGURE_MOUSE_SPEED + FIGURE_ACCELERATION;
-		}
-		else
-		{
-			speed = FIGURE_MOUSE_SPEED;
-		}
+		StopAutoMotion();
+		speed += FIGURE_MOUSE_SPEED;
 		if (GET_KEYSTATE_WPARAM(wParam) != MK_SHIFT)
 		{
 			ChangeFigureCoordinates(hWnd, 0, GET_WHEEL_DELTA_WPARAM(wParam) > 0 ? speed : -speed);
@@ -193,21 +254,12 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 			ChangeFigureCoordinates(hWnd, GET_WHEEL_DELTA_WPARAM(wParam) > 0 ? -speed : speed, 0);
 		}
-		InvalidateRect(hWnd, NULL, TRUE);
-		ShowFigureJump(hWnd);
 	}
 		break;
 	case WM_MOUSEHWHEEL:
 	{
-		int speed;
-		if (GetAsyncKeyState(VK_SPACE) < 0)
-		{
-			speed = FIGURE_MOUSE_SPEED + FIGURE_ACCELERATION;
-		}
-		else
-		{
-			speed = FIGURE_MOUSE_SPEED;
-		}
+		StopAutoMotion();
+		speed += FIGURE_MOUSE_SPEED;
 		if (GET_KEYSTATE_WPARAM(wParam) != MK_SHIFT)
 		{
 			ChangeFigureCoordinates(hWnd, GET_WHEEL_DELTA_WPARAM(wParam) > 0 ? -speed : speed, 0);
@@ -216,35 +268,34 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 			ChangeFigureCoordinates(hWnd, 0, GET_WHEEL_DELTA_WPARAM(wParam) > 0 ? speed : -speed);
 		}
-		InvalidateRect(hWnd, NULL, TRUE);
-		ShowFigureJump(hWnd);
 	}
 		break;
+	case WM_ERASEBKGND:
+		return 1;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
 	default:
-		break;
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
 	}
-	return DefWindowProc(hWnd, uMsg, wParam, lParam);
-}
-
-void InitWindowClass(WNDPROC proc, HINSTANCE hInstance, HBRUSH hBrush, const wchar_t* className) noexcept
-{
-	WNDCLASSEX wcex;
-	memset(&wcex, 0, sizeof(wcex));
-	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.lpfnWndProc = proc;
-	wcex.hInstance = hInstance;
-	wcex.hCursor = LoadCursor(0, IDC_ARROW);
-	wcex.hbrBackground = hBrush;
-	wcex.lpszClassName = className;
-	RegisterClassEx(&wcex);
+	if (verticalMotion != 0)
+	{
+		speed += FIGURE_MOUSE_SPEED;
+		ChangeFigureCoordinates(hWnd, 0, verticalMotion == INCREASE_COORDINATE ? speed : -speed);
+		Sleep(AUTO_MOTION_SLEEP);
+	}
+	if (horizontalMotion != 0)
+	{
+		speed += FIGURE_MOUSE_SPEED;
+		ChangeFigureCoordinates(hWnd, horizontalMotion == INCREASE_COORDINATE ? speed : -speed, 0);
+		Sleep(AUTO_MOTION_SLEEP);
+	}
+	return 0;
 }
 
 void CorrectFigureParameters(HWND hWnd) noexcept
 {
-	if (FIGURE_WIDTH > MAX_FIGURE_WIDTH)
+	if (FigureCoordinates.width > MAX_FIGURE_WIDTH)
 	{
 		FigureCoordinates.x = 1;
 		FigureCoordinates.width = MAX_FIGURE_WIDTH;
@@ -254,7 +305,7 @@ void CorrectFigureParameters(HWND hWnd) noexcept
 		FigureCoordinates.x = FIGURE_START_X;
 		FigureCoordinates.width = FIGURE_WIDTH + 1;
 	}
-	if (FIGURE_HEIGHT > MAX_FIGURE_HEIGHT)
+	if (FigureCoordinates.height > MAX_FIGURE_HEIGHT)
 	{
 		FigureCoordinates.y = 1;
 		FigureCoordinates.height = MAX_FIGURE_HEIGHT;
@@ -264,10 +315,24 @@ void CorrectFigureParameters(HWND hWnd) noexcept
 		FigureCoordinates.height = FIGURE_HEIGHT + 1;
 		FigureCoordinates.y = FIGURE_START_Y;
 	}
-	if (FIGURE_WIDTH > MAX_FIGURE_WIDTH || FIGURE_HEIGHT > MAX_FIGURE_HEIGHT)
+	if (FigureCoordinates.width > MAX_FIGURE_WIDTH || FigureCoordinates.height > MAX_FIGURE_HEIGHT)
 	{
-		MessageBoxW(hWnd, FIGURE_SIZE_ERROR_TEXT, FIGURE_SIZE_ERROR_CAPTION, MB_OK);
+		MessageBox(hWnd, FIGURE_SIZE_ERROR_TEXT, FIGURE_SIZE_ERROR_CAPTION, MB_OK);
 	}
+}
+
+void InitWindowClass(WNDPROC proc, HINSTANCE hInstance, HBRUSH hBrush, const wchar_t* className) noexcept
+{
+	WNDCLASSEX wcex;
+	memset(&wcex, 0, sizeof(wcex));
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.lpfnWndProc = proc;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIconW(hInstance, WINDOW_ICON);
+	wcex.hCursor = LoadCursor(0, IDC_ARROW);
+	wcex.hbrBackground = hBrush;
+	wcex.lpszClassName = className;
+	RegisterClassEx(&wcex);
 }
 
 INT WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) 
@@ -275,9 +340,13 @@ INT WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	HWND hMainWindow;
 	MSG msg;
 	InitWindowClass(WndProc, hInstance, (HBRUSH)(0), MAIN_WINDOW_CLASS);
-	hMainWindow = CreateWindowExW(0, MAIN_WINDOW_CLASS, APP_NAME, MAIN_WINDOW_STYLE, WINDOW_LEFT_BORDER, WINDOW_UPPER_BORDER, WINDOW_WIDTH, WINDOW_HEIGHT, 0, 0, hInstance, NULL);
-	
+	hMainWindow = CreateWindowEx(0, MAIN_WINDOW_CLASS, APP_NAME, MAIN_WINDOW_STYLE, WINDOW_LEFT_BORDER, WINDOW_UPPER_BORDER, WINDOW_WIDTH, WINDOW_HEIGHT, 0, 0, hInstance, NULL);
+
+	FigureCoordinates.width = FIGURE_WIDTH;
+	FigureCoordinates.height = FIGURE_HEIGHT;
 	CorrectFigureParameters(hMainWindow);
+
+	HACCEL hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
 
 	const Gdiplus::GdiplusStartupInput gdiplusStartupInput;
 	ULONG_PTR gdiplusToken;
@@ -285,8 +354,11 @@ INT WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 
 	while (GetMessage(&msg, 0, 0, 0))
 	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		if (!TranslateAccelerator(hMainWindow, hAccel, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
 	}
 
 	Gdiplus::GdiplusShutdown(gdiplusToken);
---
+++ Lab1_1/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется Lab1_1.rc
+//
+#define IDR_ACCELERATOR1                101
+#define IDI_ICON1                       102
+#define ID_BTN_UP                       40001
+#define ID_BTN_RIGHT                    40002
+#define ID_BTN_DOWN                     40003
+#define ID_BTN_LEFT                     40004
+#define ID_BTN_W                        40005
+#define ID_BTN_A                        40006
+#define ID_BTN_S                        40007
+#define ID_BTN_D                        40008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40011
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
